e68d47ee9d7d0c5df109acab3a0e0ec269cca443
vinay10082 <20bph043@nith.ac.in>
2022-10-23 21:06:52 +0530

    make better

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', async() => {
+
     const urls = ['https://www.linkedin.com/in/chetna-sharma-ab8880224',
                   'https://www.linkedin.com/in/ritik-kundlas-66a466190',
                   'https://www.linkedin.com/in/alekhgupta1441'
