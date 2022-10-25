a174658d0c922f2f44cff3c920b2d6e74712c3b5
vinay10082 <20bph043@nith.ac.in>
2022-10-25 10:57:35 +0530

    better

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 const btn = document.querySelector('.btn');
 
+
 btn.addEventListener('click', async() => {
     
     const urls = ['https://www.linkedin.com/in/purva-gautam-933645170',
     'https://www.linkedin.com/in/ritik-kundlas-66a466190',
     'https://www.linkedin.com/in/kajal-mittal-5ba86512b'
 ];
+
 for ( const url of urls) {
     await new Promise((resolve) => {
         chrome.tabs.update({url, active: true}, (tab) => {
